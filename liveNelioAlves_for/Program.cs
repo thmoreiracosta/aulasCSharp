29c53535dfc4dbd013cabc7524d2cc0749a34f99
thmoreiracosta <thiagomoreiracosta@gmail.com>
2021-03-09 22:59:39 -0300

    Adicionar arquivos de projeto.

---
+++ liveNelioAlves_for/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace liveNelioAlves_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, soma, var;
+            Console.WriteLine("Entre com o numero de vezes: ");
+            N = int.Parse(Console.ReadLine());
+            soma = 0;
+
+            for (int i=1; i<=N; i++)
+            {
+                var = int.Parse(Console.ReadLine());
+                soma += var;                
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
